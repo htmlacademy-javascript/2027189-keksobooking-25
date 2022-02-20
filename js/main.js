96e1919a3cc744f89bf94ac770cd5a24004ed044
olegshalamai <0etc@mail.ru>
2022-02-20 13:49:54 +0200

    refactored functions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
-const getRandomRangeFloatNum = (minNum, maxNum, qualityNum = 0) => {
-  if (minNum < 0) {
-    return 'Минимальное число должно быть положительным или 0.';
+const checkCorrectNumber = (value) => {
+  if (Number.isNaN(value) === true || typeof value !== 'number' || Number.isFinite(value) !== true) {
+    throw new RangeError('Числа должны быть положительными или 0.');
   }
 
-  if (maxNum < minNum) {
-    return 'Максимальное число должно быть больше минимального.';
+  if (value < 0) {
+    value = 0;
+  } else if (value > Number.MAX_SAFE_INTEGER) {
+    value = Number.MAX_SAFE_INTEGER;
   }
 
-  if (qualityNum < 0) {
-    return 'Значение количества знаков после зарятой должно быть положительным или 0.';
-  }
-
-  // Частично код взят отсюда https://habr.com/ru/company/ruvds/blog/534108/
-  return +(Math.random() * (maxNum - minNum)).toFixed(qualityNum) + minNum;
+  return value;
 };
 
+const getRandomNumber = (minNum = 0, maxNum = Number.MAX_SAFE_INTEGER, accuracy = 0) => {
+  try {
+    minNum = checkCorrectNumber(minNum);
+    maxNum = checkCorrectNumber(maxNum);
+    accuracy = checkCorrectNumber(accuracy);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error.name);
+    // eslint-disable-next-line no-console
+    console.log(error.message);
+  }
+
+
+  if (minNum > maxNum) {
+    const currentNum = minNum;
+    minNum = maxNum;
+    maxNum = currentNum;
+  }
+
+  const diff = maxNum - minNum;
+  const random = Math.random() * 1.000000000000001; // чтобы могла выпасть единица
+  const factor = Math.pow(10, accuracy);
+
+  return Math.round((random * diff + minNum) * factor) / factor;
+};
+
+
+const  getRandomUnsignedInteger = (minNum = 0, maxNum = Number.MAX_SAFE_INTEGER) => getRandomNumber(minNum, maxNum);
+
+const getRandomUnsignedFloat = (minNum = 0, maxNum = Number.MAX_SAFE_INTEGER, accuracy = 0) => getRandomNumber(minNum, maxNum, accuracy);
+
 // для ESlint
-getRandomRangeFloatNum(2, 3);
-
-
-const getRandomRangeNum = (minNum, maxNum) => getRandomRangeFloatNum(minNum, maxNum);
-
-
+getRandomUnsignedInteger(1, 4);
 // для ESlint
-getRandomRangeNum(5, 10);
+getRandomUnsignedFloat(1, 4, 3);
