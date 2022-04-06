10fb6a580236814094d9e952a18b2dca0f250465
olegshalamai <0etc@mail.ru>
2022-04-07 01:56:25 +0300

    added validate fields

--- js/forms/advertForm/gettersAdvertForm.js
+++ js/forms/advertForm/gettersAdvertForm.js
@@ -1,3 +1,4 @@
 const getValidStatus = (options, roomsComponent, capacityComponent) => options[roomsComponent.value].includes(Number(capacityComponent.value));
+const getValueSelectedElement = (selectElement) => selectElement[selectElement.selectedIndex].value;
 
-export {getValidStatus};
+export {getValidStatus, getValueSelectedElement};
