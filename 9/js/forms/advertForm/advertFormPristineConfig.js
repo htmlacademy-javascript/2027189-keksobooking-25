b62b983dcd146b813a806759ea3b550b3e76ad09
Keks <keks@htmlacademy.ru>
2022-04-09 11:56:42 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/forms/advertForm/advertFormPristineConfig.js
@@ -0,0 +1,10 @@
+const configPristineAdvertFrom = () => ({
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__message'
+});
+
+export {configPristineAdvertFrom};
---
+++ 9/js/main.js
@@ -0,0 +1,8 @@
+import {setValidateAdvertForm} from './forms/advertForm/advertForm.js';
+import {setDisabledForms} from './forms/forms.js';
+import {initMap} from './map/map.js';
+
+setDisabledForms();
+setValidateAdvertForm();
+
+initMap();
